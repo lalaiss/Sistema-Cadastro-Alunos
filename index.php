6f51ebbccd5a541374a617c1b1bb9684b90b4c87
Laís Magalhães <lais.silva13@estudante.ifto.edu.br>
2025-10-09 18:42:44 -0300

    Update Index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
                 <h2>Cadastro de Alunos</h2>
             <form action="cadastro.php" method="POST">
                     <label> Nome:</label><br><input type="text" id="nome" name="nome"  placeholder="Digite o nome do aluno" required></br><p></p>
-                    <label> Matrícula:</label><br> <input type="matricula" id="matricula" name="matricula"  placeholder="Digite a Matrícula" required></br><p></p>
+                    <label> Matrícula:</label><br><input type="text" id="matricula" name="matricula" placeholder="Digite a Matrícula" required><br><p></p>
                     <label> Curso:</label><br><input type="text" id="curso" name="curso"   placeholder="Digite o curso" required><br><p></p>
                     <label> Email:</label><br><input type="email" id="email" name="email" placeholder="Digite o e-mail"required ><br><p></p>
                     <label> Telefone:</label><br><input type="text" id="telefone" name="telefone"  placeholder="Digite o telefone" required><br><p></p>
